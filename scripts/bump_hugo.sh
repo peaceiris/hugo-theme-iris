fc767e73415003e5828f9bae1fd9d89e4320510b
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-04-20 23:39:16 +0900

    ci: Delete netlify.toml

--- scripts/bump_hugo.sh
+++ scripts/bump_hugo.sh
@@ -3,7 +3,7 @@
 # fail on unset variables and command errors
 set -eu -o pipefail # -x: is for debugging
 
-export TARGET_FILES=".env netlify.toml theme.toml"
+export TARGET_FILES=".env theme.toml"
 export HUGO_VERSION_LATEST=$(curl -s "https://api.github.com/repos/gohugoio/hugo/releases/latest" | jq -r '.tag_name')
 export HUGO_VERSION_CURRENT=$(. ./.env && echo ${HUGO_VERSION})
 echo "bump hugo from ${HUGO_VERSION_CURRENT} to ${HUGO_VERSION_LATEST#v}"
