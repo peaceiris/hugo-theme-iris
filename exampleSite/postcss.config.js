7f90dc3ac623bd63a5d2ec05fa16b9e24f026059
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-04-24 10:47:58 +0900

    feat: use PostProcess

---
+++ exampleSite/postcss.config.js
@@ -0,0 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [ './hugo_stats.json' ],
+  defaultExtractor: (content) => {
+      let els = JSON.parse(content).htmlElements;
+      return els.tags.concat(els.classes, els.ids);
+  }
+});
+
+module.exports = {
+  plugins: [
+      // require('tailwindcss'),
+      require('autoprefixer'),
+      ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
+  ]
+};
