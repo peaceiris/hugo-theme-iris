c51e07c58db8c5cb32c786ae9fa2842ce8dbf153
Shohei Ueda <30958501+peaceiris@users.noreply.github.com>
2020-09-30 14:28:54 +0900

    ci: add go.mod and go.sum (#205)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,6 @@ set -eu -o pipefail # -x: is for debugging
 cd $1
 git init
 git submodule add https://github.com/peaceiris/hugo-theme-iris.git ./themes/hugo-theme-iris
-cp -r ./themes/hugo-theme-iris/exampleSite/{config,content,data,scripts,static} .
+cp -r ./themes/hugo-theme-iris/exampleSite/{config,content,data,scripts,static,go.mod,go.sum} .
 git add .
 git commit -m "Add hugo-theme-iris"
