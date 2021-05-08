848ce8af06b34ceb3189f6409542aad01aff7daa
peaceiris <30958501+peaceiris@users.noreply.github.com>
2021-05-08 18:02:49 +0900

    Add .npmrc

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,7 +8,7 @@ git init
 wget https://github.com/peaceiris/hugo-theme-iris/archive/master.zip
 unzip master.zip
 rm master.zip
-cp -r ./hugo-theme-iris-master/exampleSite/{config,content,data,scripts,static,.gitignore,package.hugo.json} .
+cp -r ./hugo-theme-iris-master/exampleSite/{config,content,data,scripts,static,.gitignore,package.hugo.json,.npmrc} .
 rm -rf hugo-theme-iris-master
 hugo mod init "github.com/$2/$1"
 hugo mod get -u github.com/peaceiris/hugo-theme-iris
