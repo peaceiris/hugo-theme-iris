93c3ccbbaed046741e03eb41be7976f321175acf
peaceiris <30958501+peaceiris@users.noreply.github.com>
2021-05-10 20:37:56 +0900

    test: E2E testing with Jest and Playwright

---
+++ __tests__/main.test.js
@@ -0,0 +1,28 @@
+const { chromium } = require('playwright-chromium');
+const expect = require('expect');
+
+beforeAll(async () => {
+  const browser = await chromium.launch();
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+beforeEach(async () => {
+  const page = await browser.newPage();
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+describe('Keyboard Shortcut', () => {
+  test('Press g+p, go to /posts/', async () => {
+    await page.goto('https://hugothemeiris.peaceiris.app/');
+    await page.keyboard.press('g+p', {delay: 1000});
+    const postsURL = page.url();
+    expect(postsURL).toBe('https://hugothemeiris.peaceiris.app/posts/');
+    expect(await page.title()).toBe('Posts');
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  preset: 'jest-playwright-preset',
+  testMatch: ['**/*.test.js'],
+  verbose: true
+}
