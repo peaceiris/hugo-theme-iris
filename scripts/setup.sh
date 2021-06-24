7a501fae67b7152160a1216e65eb690b04f351ac
peaceiris <30958501+peaceiris@users.noreply.github.com>
2021-06-24 23:51:56 +0900

    fix: path to hugo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,9 +8,9 @@ git init
 wget https://github.com/peaceiris/hugo-theme-iris/archive/main.zip
 unzip main.zip
 rm main.zip
-./hugo mod init "github.com/$2/$1"
+hugo mod init "github.com/$2/$1"
 cp -r ./hugo-theme-iris-main/exampleSite/{assets,config,content,data,i18n,static,.gitignore} .
 rm -rf hugo-theme-iris-main
-./hugo mod get -u github.com/peaceiris/hugo-theme-iris
+hugo mod get -u github.com/peaceiris/hugo-theme-iris
 git add .
 git commit -m "Add hugo-theme-iris"
