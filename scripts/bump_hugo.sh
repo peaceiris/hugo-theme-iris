d5a73ea066ae0ab47585b1f9aaff492a79229127
peaceiris <30958501+peaceiris@users.noreply.github.com>
2022-03-24 23:10:46 +0900

    improve

--- scripts/bump_hugo.sh
+++ scripts/bump_hugo.sh
@@ -13,7 +13,7 @@ fi
 
 git pull origin "${DEFAULT_BRANCH}"
 export TARGET_FILES=".env theme.toml"
-export HUGO_VERSION_LATEST=$(curl -s "https://api.github.com/repos/gohugoio/hugo/releases/latest" | jq -r '.tag_name')
+export HUGO_VERSION_LATEST=$(curl -fsi "https://github.com/gohugoio/hugo/releases/latest" | awk -F/ '/^(L|l)ocation:/ {print $(NF)}' | tr -d '\r')
 export HUGO_VERSION_CURRENT=$(. ./.env && echo ${HUGO_VERSION})
 echo "bump hugo from ${HUGO_VERSION_CURRENT} to ${HUGO_VERSION_LATEST#v}"
 git checkout -b "hugo-${HUGO_VERSION_LATEST#v}"
@@ -21,4 +21,5 @@ sed -i "s/${HUGO_VERSION_CURRENT}/${HUGO_VERSION_LATEST#v}/g" ${TARGET_FILES}
 git add ${TARGET_FILES}
 git commit -m "deps: bump hugo from ${HUGO_VERSION_CURRENT} to ${HUGO_VERSION_LATEST#v}"
 git push origin "hugo-${HUGO_VERSION_LATEST#v}"
-gh pr create --web
+gh pr create --fill --draft
+gh pr view --web
