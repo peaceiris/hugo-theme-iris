82294bafe04b3f5953549266c372a9d05238b566
Shohei Ueda <30958501+peaceiris@users.noreply.github.com>
2022-04-24 12:27:37 +0900

    build: remove fetch_fonts.sh (#514)

--- exampleSite/scripts/fetch_fonts.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# fail on unset variables and command errors
-set -eu -o pipefail # -x: is for debugging
-
-rm -rf ./static/output/chtml/fonts/woff-v2 2>/dev/null || true
-# cp -r ./node_modules/mathjax/es5/output/chtml/fonts/woff-v2 ./static/output/chtml/fonts/woff-v2
